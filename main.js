7d6803fc2fd1486a20098358de728a9f1124b492
sagrika <sagrikathakur68@gmail.com>
2025-04-18 14:08:28 +0530

    slider

--- main.js
+++ main.js
@@ -51,10 +51,25 @@ ScrollReveal().reveal(".blog__card", {
   interval: 500,
 });
 
+// const swiper = new Swiper(".swiper", {
+//   loop: true,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
 const swiper = new Swiper(".swiper", {
   loop: true,
-
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
   },
 });
